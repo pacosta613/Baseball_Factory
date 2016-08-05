38585a272f378a2e139e610bc4a02d2da0c238b1
pacosta613 <pedro.acosta613@gmail.com>
2016-08-05 17:20:31 -0400

    Created a Divisions controller along with all its action

---
+++ app/controllers/api/divisions_controller.rb
@@ -0,0 +1,44 @@
+module Api
+
+  class DivisionsController < ApplicationController
+    respond_to :json
+
+    def index
+      respond_with(Division.all)
+    end
+
+    def create
+      @division = Division.new(division_params)
+      if @division.save
+        respond_to do |f|
+          f.json {render :json => @division}
+        end
+      end
+    end
+
+    def update
+      @division = Division.find(params[:id])
+      if @division.update(division_params)
+        respond_to do |f|
+          f.json {render :json => @division}
+        end
+      end
+    end
+
+    def show
+      respond_with(Division.find(params[:id]))
+    end
+
+    def destroy
+      respond_with(Division.destroy(params[:id]))
+    end
+
+    private
+
+    def division_params
+      params.require(:division).permit(:name)
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'application#index'
+
+  namespace :api, defaults:{:format => :json} do
+    resources :divisions, :teams, :players, :statistics
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
