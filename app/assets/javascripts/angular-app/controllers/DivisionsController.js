9f1e17a088afb42b92231a31ef4f76b51b59661c
pacosta613 <pedro.acosta613@gmail.com>
2016-08-08 14:23:31 -0400

    Added a delete divison function which allows the user to delete a division. Added a link to that function in divisions.html template.

--- app/assets/javascripts/angular-app/controllers/DivisionsController.js
+++ app/assets/javascripts/angular-app/controllers/DivisionsController.js
@@ -1,6 +1,12 @@
-function DivisionsController(Division) {
+function DivisionsController(Division, $location, $state) {
   var ctrl = this; 
   ctrl.divisions = Division.query();
+
+  ctrl.deleteDivision = function(division){
+    division.$delete(function(){
+      $state.go($state.current, {}, {reload:true});
+    });
+  };
 };
 
 angular 
