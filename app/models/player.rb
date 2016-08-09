4867b5d1adf394a36e6eb4790dd7da739b812923
pacosta613 <pedro.acosta613@gmail.com>
2016-08-09 10:09:57 -0400

    Created attr_accessible for statistic and player models. Created a method to determine the batting average and slugging percentage of each player.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,22 @@
 class Player < ActiveRecord::Base
+  attr_accessible :first_name, :last_name
+
   belongs_to :team
   has_many :statistics
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
+
+  alias :name :full_name
+
+  def batting_average
+    abs = self.statistics.map{|s|s.at_bats.to_i}.reduce(:+).to_f
+    if abs > 0
+      (self.statistics.map{|s|s.hits.to_i}.reduce(:+).to_i / abs).round(3)
+    else
+      abs
+    end
+  end
+
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,4 +1,17 @@
 class Statistic < ActiveRecord::Base
+  attr_accessible :player_id, :team_id, :games, :at_bats, :runs, :homeruns, :hits, :singles, :doubles, :triples, :rbis, :stolen_bases, :caught_stealing
+
   belongs_to :player
   belongs_to :team
+
+  def batting_average
+    abs = self.at_bats.to_f
+    abs > 0 ? (self.hits.to_f / abs).round(3) : 0
+  end
+
+  def slugging_pct
+    abs = self.at_bats.to_f
+    abs > 0 ? ([(self.hits.to_i - self.doubles.to_i - self.triples.to_i - self.homeruns.to_i),(2 * self.doubles.to_i),(3 * self.triples.to_i),(4 * self.homeruns.to_i)].inject(:+) / abs).round(3) : 0
+  end
+  
 end
