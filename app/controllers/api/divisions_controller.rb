61e94629808d2836449eb9b2f9f238d537ae90c4
pacosta613 <pedro.acosta613@gmail.com>
2016-08-10 17:58:43 -0400

    Changed one line of code in the divisions controller show action to find teams with division_id.

--- app/controllers/api/divisions_controller.rb
+++ app/controllers/api/divisions_controller.rb
@@ -26,7 +26,7 @@ module Api
 
     def show
       @team = Team.new
-      @division = Division.find(params[:id])
+      @division = Division.find_by(params[:division_id])
       @teams = @division.teams
       respond_with(@division)
     end
